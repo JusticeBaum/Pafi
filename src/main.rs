87da6b3bd225fd7d512abbb7fb7e064d04ee8b0b
Justice Baum <justicebaum12@gmail.com>
2024-04-12 14:31:00 -0700

    Basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use std::io::{BufRead, BufReader};
+use std::{fs::File, path::PathBuf};
+
+#[derive(Parser)]
+struct Args {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let f = File::open(args.path).expect("Invalid file path");
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+    while reader.read_line(&mut line).unwrap() > 0 {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+            return
+        }
+        line.clear();
+    }
 }
